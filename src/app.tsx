4c7f5f8d106bfaabd8f6a78c6b988e02dbdd6e99
miizzo <miizzo.js@mail.ru>
2020-11-02 00:33:48 +0300

    feat: created a header component and render it in App

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "./common/molecules/header";
 
 const App = () => {
     return (
-        <div>hello</div>
+        <>
+            <Header />
+        </>
     );
 }
 
---
+++ src/common/molecules/header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import { useStyles } from "./header.styles";
+import { AccountCircle } from "@material-ui/icons";
+
+const Header = () => {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    Spotify
+                </Typography>
+                <IconButton>
+                    <AccountCircle />
+                </IconButton>
+                <Typography variant="h6">MiiZZo</Typography>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header;
