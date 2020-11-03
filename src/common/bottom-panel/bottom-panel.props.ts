0273bd27dde4feadc39a15013afe4de87112c135
miizzo <miizzo.js@mail.ru>
2020-11-03 20:05:52 +0300

    chore: updated bottom panel

--- src/common/bottom-panel/bottom-panel.props.ts
+++
@@ -1,7 +0,0 @@
-import { TrackDTO } from "../../typings/track.dto";
-
-export interface BottomPanelProps {
-    track: TrackDTO;
-    handleChooseNextTrack: () => void;
-    handleChoosePrevTrack: () => void;
-}
--- src/common/bottom-panel/bottom-panel.tsx
+++ src/common/bottom-panel/bottom-panel.tsx
@@ -1,20 +1,14 @@
-import { Box, Typography } from "@material-ui/core";
-import { MusicNote } from "@material-ui/icons";
+import { Box } from "@material-ui/core";
 import React from "react";
 import Player from "../../features/player/player";
 import { useStyles } from "./bottom-panel.styles";
-import { BottomPanelProps } from "./bottom-panel.props";
 
-const BottomPanel = ({ track, handleChooseNextTrack, handleChoosePrevTrack }: BottomPanelProps) => {
+const BottomPanel = () => {
     const classes = useStyles();
 
     return (
         <Box className={classes.bottomPanel}>
-            <Player 
-                track={track} 
-                handleChooseNextTrack={handleChooseNextTrack} 
-                handleChoosePrevTrack={handleChoosePrevTrack} 
-            />
+            <Player />
         </Box>
     );
 }
