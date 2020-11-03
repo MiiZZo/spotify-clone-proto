af35b41535bce182bf17d4cccc6a76dfa31615da
miizzo <miizzo.js@mail.ru>
2020-11-03 20:02:55 +0300

    feat: provided player store to app as context

--- src/app.tsx
+++ src/app.tsx
@@ -1,67 +1,31 @@
 import { Box } from "@material-ui/core";
+import { observer } from "mobx-react-lite";
 import React, { useState } from "react";
 import { useStyles } from "./app.styles";
 import BottomPanel from "./common/bottom-panel/bottom-panel";
 import Header from "./common/molecules/header";
-import Player from "./features/player/player";
+import { PlayerStore } from "./features/player/player.store";
 //@ts-ignore
 import audio from "./track.mp3";
 //@ts-ignore
 import audio2 from "./track2.mp3";
 //@ts-ignore
 import audio3 from "./track3.mp3";
-import { TrackDTO } from "./typings/track.dto";
+import { PlayerStoreContext } from "./features/player/player-store.context";
 
-const App = () => {
-    const classes = useStyles();
-
-    const playlist: TrackDTO[] = [
-        {
-            audio: audio,
-            authorName: 'Myosuki',
-            title: 'My sound',
-        },
-        {
-            audio: audio2,
-            authorName: 'Tyosuki',
-            title: 'Your sound',
-        },
-        {
-            audio: audio3,
-            authorName: 'Zyosuki',
-            title: 'My name',
-        }
-    ];
-
-    const [currentTrack, setCurrentTrack] = useState(0);
-    
-    const handleChoosePrevTrack = () => {
-        if (currentTrack === 0) {
-            setCurrentTrack(0);
-        } else {
-            setCurrentTrack(currentTrack - 1);
-        }
-    }
-
-    const handleChooseNextTrack = () => {
-        if (currentTrack === playlist.length - 1) {
-            setCurrentTrack(0);
-        } else {
-            setCurrentTrack(currentTrack + 1);
-        }
-    }
+const App = observer(() => {
+    const [playerStore] = useState(() => new PlayerStore([audio, audio2, audio3]));
+    const classes = useStyles();    
 
     return (
-        <Box height="100%" className={classes.root}>
-            <Header />
-            <Box></Box>
-            <BottomPanel
-                handleChoosePrevTrack={handleChoosePrevTrack}
-                handleChooseNextTrack={handleChooseNextTrack}
-                track={playlist[currentTrack]} 
-            />
-        </Box>
+        <PlayerStoreContext.Provider value={playerStore}>        
+            <Box height="100%" className={classes.root}>
+                <Header />
+                <Box></Box>
+                <BottomPanel />
+            </Box>
+        </PlayerStoreContext.Provider>
     );
-}
+});
 
 export default App;
