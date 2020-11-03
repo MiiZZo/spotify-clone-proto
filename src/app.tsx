c4e51b0bb62235adfe9ab188fd75cd04f0d55a6e
miizzo <miizzo.js@mail.ru>
2020-11-03 22:34:07 +0300

    chore: provided playlist store to app as context

--- src/app.tsx
+++ src/app.tsx
@@ -5,27 +5,32 @@ import { useStyles } from "./app.styles";
 import BottomPanel from "./common/bottom-panel/bottom-panel";
 import Header from "./common/molecules/header";
 import { PlayerStore } from "./features/player/player.store";
-//@ts-ignore
-import audio from "./track.mp3";
-//@ts-ignore
-import audio2 from "./track2.mp3";
-//@ts-ignore
-import audio3 from "./track3.mp3";
 import { PlayerStoreContext } from "./features/player/player-store.context";
+import PlaylistPage from "./features/playlist/playlist-page";
+import { PlaylistStore } from "./features/playlist/playlist.store";
+import { PlaylistStoreContext } from "./features/playlist/playlist-store.context";
 
 const App = observer(() => {
-    const [playerStore] = useState(() => new PlayerStore([audio, audio2, audio3]));
-    const classes = useStyles();    
+  const [playlistStore] = useState(() => new PlaylistStore());
+  const [playerStore] = useState(
+    () =>
+      new PlayerStore(
+        playlistStore.playlists[0].tracks.map((track) => track.audio)
+      )
+  );
+  const classes = useStyles();
 
-    return (
-        <PlayerStoreContext.Provider value={playerStore}>        
-            <Box height="100%" className={classes.root}>
-                <Header />
-                <Box></Box>
-                <BottomPanel />
-            </Box>
-        </PlayerStoreContext.Provider>
-    );
+  return (
+    <PlaylistStoreContext.Provider value={playlistStore}>
+      <PlayerStoreContext.Provider value={playerStore}>
+        <Box height="100%" className={classes.root}>
+          <Header />
+          <PlaylistPage />
+          <BottomPanel />
+        </Box>
+      </PlayerStoreContext.Provider>
+    </PlaylistStoreContext.Provider>
+  );
 });
 
 export default App;
