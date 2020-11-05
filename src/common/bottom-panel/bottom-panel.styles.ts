6fcdec73728c82d60b490fffd3558711c04997c7
miizzo <miizzo.js@mail.ru>
2020-11-05 17:54:46 +0300

    feat: updated header component

--- src/common/bottom-panel/bottom-panel.styles.ts
+++ src/common/bottom-panel/bottom-panel.styles.ts
@@ -4,14 +4,15 @@ export const useStyles = makeStyles((theme) =>
     createStyles({
         bottomPanel: {
             height: 80,
-            background: theme.palette.primary.light,
+            background: theme.palette.primary.main,
             width: "100%",
             position: "fixed",
             left: 0,
             bottom: 0,
             display: "flex",
             alignItems: "center",
-            padding: 15
+            padding: 15,
+            boxShadow: '0px 0px 1px black'
         }
     })
 );
