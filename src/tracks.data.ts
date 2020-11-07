5f4d7a27a2803208d33398b73440ef6d2298b5a6
miizzo <=>
2020-11-07 19:45:49 +0300

    refactor: moved mp3 files to public folder

--- src/tracks.data.ts
+++ src/tracks.data.ts
@@ -1,27 +1,4 @@
 import { TrackDTO } from "./typings/track.dto";
-//@ts-ignore
-import track from "./track.mp3";
-//@ts-ignore
-import track2 from "./track2.mp3";
-//@ts-ignore
-import track3 from "./track3.mp3";
-//@ts-ignore
-import track4 from "./track4.mp3";
-//@ts-ignore
-import track5 from "./track5.mp3";
-//@ts-ignore
-import track6 from "./track6.mp3";
-//@ts-ignore
-import track7 from "./track7.mp3";
-//@ts-ignore
-import f1 from "./f1.mp3";
-//@ts-ignore
-import f2 from "./f2.mp3";
-//@ts-ignore
-import f3 from "./f3.mp3";
-//@ts-ignore
-import f4 from "./f4.mp3";
-
 
 const makeTrack = (title: string, author: string, duration: string, audio: any) => (
     {
@@ -40,67 +17,67 @@ export const tracks: TrackDTO[] = [
         "Song 1",
         "Artist 1",
         "1:25",
-        track
+        `${process.env.PUBLIC_URL}/mp3/track.mp3`
     ),
     makeTrack(
         "Song 2",
         "Artist 2",
         "2:25",
-        track2
+        `${process.env.PUBLIC_URL}/mp3/track2.mp3`
     ),
     makeTrack(
         "Song 3",
         "Artist 3",
         "3:25",
-        track3
+        `${process.env.PUBLIC_URL}/mp3/track3.mp3`
     ),
     makeTrack(
         "Song 4",
         "Artist 4",
         "1:75",
-        track4
+        `${process.env.PUBLIC_URL}/mp3/track4.mp3`
     ),
     makeTrack(
         "Song 5",
         "Artist 5",
         "1:43",
-        track5
+        `${process.env.PUBLIC_URL}/mp3/track5.mp3`
     ),
     makeTrack(
         "Song 6",
         "Artist 6",
         "1:325",
-        track6
+        `${process.env.PUBLIC_URL}/mp3/track6.mp3`
     ),
     makeTrack(
         "Song 7",
         "Artist 7",
         "2:35",
-        track7
+        `${process.env.PUBLIC_URL}/mp3/track7.mp3`
     ),
 //
     makeTrack(
         "Song 7",
         "Ryo Fukui",
         "2:35",
-        f1
+        `${process.env.PUBLIC_URL}/mp3/f1.mp3`
     ),
     makeTrack(
         "Song 7",
         "Ryo Fukui",
         "2:35",
-        f2
+        `${process.env.PUBLIC_URL}/mp3/f2.mp3`
     ),
     makeTrack(
         "Song 7",
         "Ryo Fukui",
         "2:35",
-        f3
+        `${process.env.PUBLIC_URL}/mp3/f3.mp3`
     ),
     makeTrack(
         "Song 7",
         "Ryo Fukui",
         "2:35",
-        f4
+        `${process.env.PUBLIC_URL}/mp3/f4.mp3`
     ),
 ];
